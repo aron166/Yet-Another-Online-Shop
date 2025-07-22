ad619587e7dc9ca2b3af3a16c1caeaad5c00ff98
Áron <balogh.aron16@gmail.com>
2025-07-22 15:12:07 +0200

    merge

--- client/script.js
+++ client/script.js
@@ -1,6 +1,6 @@
 async function fetchDatas() {
     try {
-        const response = await fetch(`/api/product/:1`);
+        const response = await fetch(`/api/product/3`);
         if (!response.ok) throw new Error("Fetch failed", response.status);
 
         const data = await response.json();
