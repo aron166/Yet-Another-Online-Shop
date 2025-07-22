bc5ff74797acba81e4273c9d936b9089099cdaf9
DSaca95 <dalmadisanyi@gmail.com>
2025-07-22 11:31:10 +0200

    frontend_development

--- client/script.js
+++ client/script.js
@@ -1,6 +1,6 @@
 async function fetchDatas() {
     try {
-        const response = await fetch(`/api/product/:1`);
+        const response = await fetch(`/api/products`);
         if (!response.ok) throw new Error("Fetch failed", response.status);
 
         const data = await response.json();
@@ -10,8 +10,37 @@ async function fetchDatas() {
     }
 }
 
+function addNavButtons () {
+    const navBarDiv = document.getElementById('header');
+    const homeButton = document.createElement('button');
+    homeButton.textContent = 'Home';
+    homeButton.id = 'homeButton';
+    navBarDiv.appendChild(homeButton);
+    const cartBtn = document.createElement('button');
+    cartBtn.textContent = 'Cart';
+    cartBtn.id = 'cart';
+    navBarDiv.appendChild(cartBtn);
+    const adminPublicBtn = document.createElement('button');
+    adminPublicBtn.textContent = 'Admin Mode';
+    adminPublicBtn.id = 'admin-public';
+    navBarDiv.appendChild(adminPublicBtn);
+}
+ function showcart() {
+  const sections = document.querySelectorAll("section");
+  sections.forEach(sec => sec.style.display = "none");
+  document.getElementById('cart').style.display = "block";
+}
+function showHomePage(){
+    const section = document.querySelectorAll("section");
+    section.forEach(sec => sec.style.display = 'none');
+    document.getElementById('slideshow').style.display = 'block';
+    document.getElementById('product-list').style.display = 'block';
+}
+
 async function main() {
-  fetchDatas();  
+  await fetchDatas();  
+  addNavButtons();
+
 }
 main();
 //window.addEventListener('load', main);
